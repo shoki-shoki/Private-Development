bcdd4f8b126c5b8e7fcef113d6908a6d455f7872
shoki <shoki.fukasawa@gmail.com>
2025-02-25 23:22:48 +0900

    spring_foodのモーダルウィンドウが閉じられるようになった段階

---
+++ 四季折々/js/spring-food.js
@@ -0,0 +1,15 @@
+
+const modal = document.querySelector('.js-modal');
+const modalButton = document.querySelector('.js-modal-button1');
+
+// 追記
+const modalClose = document.querySelector('.js-close-button1');　// xボタンのjs-close-buttonを取得し変数に格納
+
+modalButton.addEventListener('click', () => {
+  modal.classList.add('is-open');
+});
+
+// 追記
+modalClose.addEventListener('click', () => { // xボタンをクリックしたときのイベントを登録
+  modal.classList.remove('is-open'); 
+});
