32e213d112e196a7c2a7e110e303bb81de392067
shoki <shoki.fukasawa@gmail.com>
2025-02-27 00:49:53 +0900

    spring_htmlの中身整理＆モーダルウィンドウフェードイン、目次アニメーション追加した段階

--- 四季折々/js/spring-food.js
+++ 四季折々/js/spring-food.js
@@ -1,5 +1,5 @@
 
-const modal = document.querySelector('.js-modal');
+const modal = document.querySelector('.js-modal1');
 const modalButton = document.querySelector('.js-modal-button1');
 
 // 追記
@@ -12,4 +12,23 @@ modalButton.addEventListener('click', () => {
 // 追記
 modalClose.addEventListener('click', () => { // xボタンをクリックしたときのイベントを登録
   modal.classList.remove('is-open'); 
+});
+
+
+
+
+
+const modal2 = document.querySelector('.js-modal2');
+const modalButton2 = document.querySelector('.js-modal-button2');
+
+// 追記
+const modalClose2 = document.querySelector('.js-close-button2');　// xボタンのjs-close-buttonを取得し変数に格納
+
+modalButton2.addEventListener('click', () => {
+  modal2.classList.add('is-open');
+});
+
+// 追記
+modalClose2.addEventListener('click', () => { // xボタンをクリックしたときのイベントを登録
+  modal2.classList.remove('is-open'); 
 });
